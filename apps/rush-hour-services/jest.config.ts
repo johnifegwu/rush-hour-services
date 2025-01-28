0461601abd3fd03327ea9738d7ff63acd513ebc7
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2025-01-28 14:13:35 +0100

    Initial commit

---
+++ apps/rush-hour-services/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'rush-hour-services',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/rush-hour-services',
+};
