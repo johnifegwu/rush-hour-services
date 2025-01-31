485baf1d1f3e7e9b400dc64ec94a4ddc098979d8
John Ifegwu <57634092+johnifegwu@users.noreply.github.com>
2025-01-31 02:35:19 +0100

    gateway running

--- apps/gateway/webpack.config.js
+++ apps/gateway/webpack.config.js
@@ -1,19 +1,23 @@
 const { composePlugins, withNx } = require('@nx/webpack');
-const { join } = require('path');
 
 module.exports = composePlugins(withNx(), (config) => {
-  // Update the webpack config
-  config.output = {
-    path: join(__dirname, '../../dist/apps/gateway'),
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+      ],
+    },
+    resolve: {
+      ...config.resolve,
+      fallback: {
+        ...config.resolve.fallback,
+      },
+    },
+    optimization: {
+      ...config.optimization,
+      moduleIds: 'named',
+    },
   };
-
-  // Set target to node since this is a Node.js application
-  config.target = 'node';
-
-  // Disable optimization
-  config.optimization = {
-    minimize: false
-  };
-
-  return config;
 });
